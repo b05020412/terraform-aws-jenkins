fc38c44f39f37bbc3503db5bdc3409bec382f015
Sandeep Boddu <boddusandeep23@gmail.com>
2019-08-19 13:04:02 +0000

    Formatting in local.tf

--- local.tf
+++ local.tf
@@ -1,23 +1,23 @@
 locals {
-  
+
   default_master_config = {
-    instance_family  = var.default_master_instance_family
-    root_ebs_size    = var.default_master_root_ebs_size
-    ami              = coalesce(var.default_master_ami, data.aws_ami.amazonlinux.id)
-    subnet_id        = coalesce(var.default_master_subnet_id, var.subnets[0])
-    root_volume_type = var.default_master_root_volume_type
-    additional_sg_list =[]
-    keypair_name = var.keypair_name
+    instance_family    = var.default_master_instance_family
+    root_ebs_size      = var.default_master_root_ebs_size
+    ami                = coalesce(var.default_master_ami, data.aws_ami.amazonlinux.id)
+    subnet_id          = coalesce(var.default_master_subnet_id, var.subnets[0])
+    root_volume_type   = var.default_master_root_volume_type
+    additional_sg_list = []
+    keypair_name       = var.keypair_name
   }
 
   default_slave_config = {
-    instance_family  = var.default_slave_instance_family
-    root_ebs_size    = var.default_slave_root_ebs_size
-    ami              = coalesce(var.default_slave_ami, data.aws_ami.amazonlinux.id)
-    subnet_id        = coalesce(var.default_slave_subnet_id, var.subnets[0])
-    root_volume_type = var.default_slave_root_volume_type
-    additional_sg_list=[]
-    keypair_name =var.keypair_name
+    instance_family    = var.default_slave_instance_family
+    root_ebs_size      = var.default_slave_root_ebs_size
+    ami                = coalesce(var.default_slave_ami, data.aws_ami.amazonlinux.id)
+    subnet_id          = coalesce(var.default_slave_subnet_id, var.subnets[0])
+    root_volume_type   = var.default_slave_root_volume_type
+    additional_sg_list = []
+    keypair_name       = var.keypair_name
   }
 
   master_config = [for item in var.master_config : merge(local.default_master_config, item)]
@@ -35,12 +35,12 @@ locals {
   target_groups_count = 1
 
   target_groups = [
-        {
-        "name"             = "jenkins"
-        "backend_protocol" = "HTTP"
-        "backend_port"     = 8080
-        "slow_start"       = 100
-        },
+    {
+      "name"             = "jenkins"
+      "backend_protocol" = "HTTP"
+      "backend_port"     = 8080
+      "slow_start"       = 100
+    },
   ]
 
 }
